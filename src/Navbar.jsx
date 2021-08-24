d6f3a6afd6c3265fb596e21f8070430eedcdd1df
codewithmuskan <86367597+codewithmuskan@users.noreply.github.com>
2021-08-24 22:32:20 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -17,6 +17,9 @@ const Navbar=()=>{
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+         <li className="nav-item">
+          <NavLink className="nav-link active" aria-current="page" to="#">🍄Home</NavLink>
+        </li>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
          <li className="nav-item">
