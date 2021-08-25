4acf85ff7b5e0de4ba8ecbebf9eccc284ccd61b1
codewithmuskan <86367597+codewithmuskan@users.noreply.github.com>
2021-08-25 12:49:18 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar=()=>{
                 
            <nav class="navbar navbar-expand-lg navbar-light bg-white">
   <div className="container-fluid">
-    <NavLink className="navbar-brand" to="#"> 💖Muskan Katiyar</NavLink>
+    <NavLink className="navbar-brand" to="/"> 💖Muskan Katiyar</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -21,7 +21,7 @@ const Navbar=()=>{
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
          <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="#">🍄Home</NavLink>
+          <NavLink className="nav-link active" aria-current="page" to="/">🍄Home</NavLink>
         </li>
        
         <li className="nav-item">
