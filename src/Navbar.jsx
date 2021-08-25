6b8c754a47b5990cbe125e3728ac1b2c77ea1ab6
codewithmuskan <86367597+codewithmuskan@users.noreply.github.com>
2021-08-25 13:06:11 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -16,12 +16,14 @@ const Navbar=()=>{
     <NavLink className="navbar-brand" to="/"> 💖Muskan Katiyar</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
-         <NavLink className="nav-link active" aria-current="page" to="/">🍄Home</NavLink>
+       
     </button>
         
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-         
+         <li className="nav-item">
+          <NavLink className="nav-link active" aria-current="page" to="/">🍄Home</NavLink>
+        </li>
        
         <li className="nav-item">
           <NavLink className="nav-link" to="/Education">🌏Education</NavLink>
