66be149c4ce864a752cd5ef43bf9c4fa6a0403d1
codewithmuskan <86367597+codewithmuskan@users.noreply.github.com>
2021-08-25 13:04:40 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,31 +13,30 @@ const Navbar=()=>{
                 
            <nav class="navbar navbar-expand-lg navbar-light bg-white">
   <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/react-tutorial/"> 💖Muskan Katiyar</NavLink>
+    <NavLink className="navbar-brand" to="/"> 💖Muskan Katiyar</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
+         <NavLink className="nav-link active" aria-current="page" to="/">🍄Home</NavLink>
     </button>
         
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-         <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="/react-tutorial/">🍄Home</NavLink>
-        </li>
+         
        
         <li className="nav-item">
-          <NavLink className="nav-link" to="/react-tutorial/Education">🌏Education</NavLink>
+          <NavLink className="nav-link" to="/Education">🌏Education</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/react-tutorial/Experience">✏️Experience</NavLink>
+          <NavLink className="nav-link" to="/Experience">✏️Experience</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/react-tutorial/Projects">📝Projects</NavLink>
+          <NavLink className="nav-link" to="/Projects">📝Projects</NavLink>
         </li>
         {/* <li className="nav-item">
           <NavLink className="nav-link" to="/Open"> ✨Open source</NavLink>
         </li> */}
         <li className="nav-item">
-          <NavLink className="nav-link" to="/react-tutorial/Contact">📲Contact Me</NavLink>
+          <NavLink className="nav-link" to="/Contact">📲Contact Me</NavLink>
         </li>
        
       </ul>
